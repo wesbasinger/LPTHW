3eaa5e5e6780fb63b7f926e7e88c6c02aba5a7ad
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-07 09:56:47 -0500

    Curled all the html into folders and touched  interactive, practice, validate for each folder.

---
+++ ex0/practice.py
@@ -0,0 +1,33 @@
+# You actually won't be writing any Python code in this exercise,
+# instead you will be doing some of the directory and command line
+# tasks that are listed in the exercise.  After you're done, type
+# "python validate.py" (without the quotes) on a command line or
+# terminal window to check yourself.  Next lesson will explain how
+# to execute programs more.
+
+# TASK 1
+# make a directory inside this "ex0" folder called "dir_practice"
+
+# TASK 2
+# change into this directory and make a file calleed "file_practice.txt"
+
+# TASK 3
+# put the following text between the triple quotes into that file.  
+# Make sure you get it exactly the same, line breaks and all.
+
+'''
+1.  Get a suitable text editor.
+2.  Make sure you can get to your text editor quickly.
+3.  Run PowerShell or Terminal.
+4.  In that terminal window, type "python".
+5.  Type "quit()" or "exit".
+6.  Learn how to make a directory.
+7.  Learn how to change into a directory.
+8.  Learn how to save files in your text editor.
+9.  Get comfortable with the command line.
+'''
+
+# You're done!  You should be able to validate your progress by typing
+# "python validate.py".  If you get errors, something is probably wrong.
+
+
---
+++ ex0/validate.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import unittest
+
+file_practice = os.path.join('dir_practice', 'file_practice.txt')
+
+
+try:
+	f = open(file_practice, 'r')
+	print "File opened successfully --- \n Dir and file created."
+except IOError:
+	print "File not opened sucessfully -- \n Check the directions."
+	sys.exit(1)
+	
+
+should_string = '''1.  Get a suitable text editor.
+2.  Make sure you can get to your text editor quickly.
+3.  Run PowerShell or Terminal.
+4.  In that terminal window, type "python".
+5.  Type "quit()" or "exit".
+6.  Learn how to make a directory.
+7.  Learn how to change into a directory.
+8.  Learn how to save files in your text editor.
+9.  Get comfortable with the command line.
+'''
+
+class TestFileAndDirectoryCreation(unittest.TestCase):
+
+  def test_file_matches(self):
+      self.assertEqual(f.read(), should_string,
+		msg="The text from the files does not match exactly...")
+
+if __name__ == '__main__':
+    unittest.main()
