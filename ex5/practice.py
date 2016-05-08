4d3f611001ccadc686c7d60e9c72614470e4f390
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-08 18:06:04 -0500

    Added the code for practice.

---
+++ ex5/practice.py
@@ -0,0 +1,22 @@
+# From my point of view (fairly novice) this is almost
+# a depreciated method.  String formatting in Python 3 is
+# very different.  Nonetheless, it's a good way to think
+# about interpolating variables into strings and you can
+# still practice it.
+
+# Finish the following variable declarations below.
+
+my_name = 
+my_age =
+my_height =
+my_weight = 
+my_eyes =
+my_teeth =
+my_hair =
+
+print "Let's talk about %s." % PUT_MY_NAME_VARIABLE_HERE
+print "He's %s inches tall." % PUT_MY_HEIGHT_VARIABLE_HERE
+print "He's %s pounds heavy." % PUT_MY_WEIGHT_VARIABLE_HERE
+print "He's got %s eyes and %s hair." % (PUT_APPROPRIATE_VARIABLES HERE)
+print "His teeth are usually %s depending on the coffee." % PUT_APPROPRIATE_VARAIBLE HERE
+print "If I add %d, %d, and %d I get %d." % PUT_APPROPRIATE VARIABLES HERE
