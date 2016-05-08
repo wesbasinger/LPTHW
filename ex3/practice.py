163a87affb845d54cb6d5ce06c4039362d9c2cab
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-08 16:29:58 -0500

    Added the empty practice file.

---
+++ ex3/practice.py
@@ -0,0 +1,18 @@
+# For this practice drill, add a math expression on each line
+# to make the line print the given value.
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print 6
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print -3
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print 100, use multiplication
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print True
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print 4, use 3 different operations
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print 1, use modulus
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print 1.23
+
+print REPLACE_WITH_MATH_EXPRESSION # make this line print False, use at least 3 different operations
