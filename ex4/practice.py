bc9ee916ecb96d6ba4530945a81c9865a159bcef
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-08 17:24:55 -0500

    Made the practice file.

---
+++ ex4/practice.py
@@ -0,0 +1,26 @@
+# Using your new knowledge of variables, modify the code
+# so that it prints what the comments call for.
+
+capital_city_of_Texas =				# Remember to protect strings with quotes!!!
+
+# should print "The capital city of Texas is Austin."
+print "The capital city of Texas is " + capital_city_of_Texas + "."
+
+num_sides_of_a_square =
+
+# should print "The number of sides in a square is 4."
+print "The number of sides in a square is " + str(num_sides_of_a_square) + "."
+
+water_is_a_molecule =
+
+# should print "Is water a molecule? True"
+print "Is water a molecule? " + str(water_is_a_molecule)
+
+num_sides_of_a_triangle = 
+
+# should print "A triangle has 1 less side than a square."
+print "A triangle has " + str(num_sides_of_a_square - num_sides_of_a_triangle) + " less side than a square."
+
+marys_pet = 
+# should print "Mary's pet was a little lamb."
+print "Mary's pet was a little " + marys_pet + "."
