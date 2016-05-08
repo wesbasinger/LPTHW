de44e06da0a39fe899430fcd890d8bf8f948f953
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-07 22:21:00 -0500

    Initial code for practice file.  Decided against interactive for this lesson.

---
+++ ex2/practice.py
@@ -0,0 +1,17 @@
+# Modify the following code so that it prints
+# 'Monty Python and the Flying Circus'
+
+# Try to do this by only adding and removing hashtags.
+
+print "This is unrelated content that I don't want to print."
+
+This is just a random comment about life in general, I really
+don't want any of this to print.
+
+# print "Monty "
+
+print "Python " and the Search for the Holy Grail
+
+# print "and the Flying " Bears
+
+print "Circus"
