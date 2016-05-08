a8a9f83452dad3a3da59b6afd3e799024509b6ed
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-07 22:10:42 -0500

    And that should do it for ex1.

--- ex1/practice.py
+++ ex1/practice.py
@@ -1,7 +1,7 @@
 # In this exercise, we will do practice with only the
 # print statement.  Just read the commented lines and
-# put a print statement to print exactly what is in the
-# single quotes.
+# put a print statement to print exactly what is
+# after the pound sign.
 
 # Your mother was a hamster and your father smelt of elderberries!
 
@@ -9,15 +9,17 @@
 # All right... all right... but apart from better sanitation, the medicine, education, wine, public order, irrigation, roads, a fresh water system, and public health ... what have the Romans ever done for us?
 
 
-# Please, this is supposed to be a happy occasion. Let us not bicker and argue over who killed who.
+#  Please, this is supposed to be a happy occasion. Let us not bicker and argue over who killed who.
+
 
 
 # Listen, strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.
 
 
-# 'I’m sorry to have kept you waiting, but I’m afraid my walk has become rather sillier recently.'
+
+# I'm sorry to have kept you waiting, but I'm afraid my walk has become rather sillier recently.
 
 
-# 'It's just a flesh wound.'
 
+# It's just a flesh wound.
 
