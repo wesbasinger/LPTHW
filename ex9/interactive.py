c5b6dca7a20743cc6791c8951eb032763d557bc0
classroom4 <wbasinger@village.techschools.org>
2016-05-09 10:28:00 -0500

    Started on the interactive portion.

---
+++ ex9/interactive.py
@@ -0,0 +1,19 @@
+from sys import exit
+
+print "Bet you're sick of printing by now!"
+print "But wait, there's more."
+
+print "This lesson introduces two important concepts,"
+print "the newline character and the multi line print statement."
+print "I've seen the multi line print statement used more for"
+print "long comments and docstrings, but it's still good to learn"
+print "like this."
+
+print "First, input this command, I'm not really going to check your"
+print "basic inputs from now on, I think you should have those down"
+print "by now."
+print r'print "Hello\n"'
+prompt = raw_input(">>>")
+
+print "Did you see how it put in a new line?"
+print "The
