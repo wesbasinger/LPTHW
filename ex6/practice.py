80c5d5c8f116d06815d25b5df7f8989f039b9834
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-08 21:29:34 -0500

    Just started on the string concat lesson.

---
+++ ex6/practice.py
@@ -0,0 +1,16 @@
+# I'll focus mostly on string concatenation.
+# Honestly, I've never used the varied types of
+# string formatting, although I'm sure they're useful.
+
+# Make the console print the first line of Mary Had a Little Lamb
+# Do not make any new strings, only use concatenation
+
+main_char = "Mary"
+space = " "
+verb = "had"
+indef_art = "a"
+adjective = "little"
+direct_object = "lamb"
+
+# should print 'Mary had a little lamb'
+print YOUR CODE GOES HERE
