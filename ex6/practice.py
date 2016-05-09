57b52531e61217125f407f4fa25e8e4826dd49d3
classroom4 <wbasinger@village.techschools.org>
2016-05-09 09:09:22 -0500

    Added just a little bit more.

--- ex6/practice.py
+++ ex6/practice.py
@@ -5,6 +5,15 @@
 # Make the console print the first line of Mary Had a Little Lamb
 # Do not make any new strings, only use concatenation
 
+first_part = 
+
+second_part = 
+
+# should print 'Row, row, row your boat Gently down the stream."
+# ***please note*** the space is added for you
+print first_part + " " + second_part
+
+
 main_char = "Mary"
 space = " "
 verb = "had"
@@ -14,3 +23,9 @@ direct_object = "lamb"
 
 # should print 'Mary had a little lamb'
 print YOUR CODE GOES HERE
+
+one = "one"
+one_digit = "1"
+
+# should print 'Takes 1 to know one."
+print "Takes " + XXX + " to know " + XXX + "."
