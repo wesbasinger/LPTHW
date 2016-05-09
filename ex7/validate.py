4c333e4fefbe6d24cdd751c4343bb8a63622a19f
classroom4 <wbasinger@village.techschools.org>
2016-05-09 10:16:56 -0500

    Fixed a incongruency.

--- ex7/validate.py
+++ ex7/validate.py
@@ -4,14 +4,14 @@ from practice import *
 
 class TestCanUseStringFormatting(unittest.TestCase):
 
-    def test_can_Pounce_3_times(self):
+    def test_can_Pounce_4_times(self):
 		self.assertEqual(
 			word,
 			"Pounce"
 		)
 		self.assertEqual(
 			combined,
-			"PouncePouncePounce"
+			"PouncePouncePouncePounce"
 		)
 
     def test_can_make_plain_line_sep(self):
