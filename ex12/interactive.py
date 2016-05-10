0c5438c8b85f0cea3bf98c58cfa0f7cfab576bb4
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-10 18:50:33 -0500

    Can't think of anything to really do on these prompt lessons.

---
+++ ex12/interactive.py
@@ -0,0 +1,5 @@
+print "So, I can't really test that you can do"
+print "this set of skills... or at least I haven't"
+print "figured out yet.  Keep forging ahead."
+print "Input is useful, but also very easy and"
+print "usually works the way you expect it to."
