8211cb5dedc049fa172975de59184c458a1d0b13
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-10 18:42:01 -0500

    Looks good, simple though.

---
+++ ex11/interactive.py
@@ -0,0 +1,18 @@
+print "This lesson is about prompting."
+print "Actually, that's what I've been doing all"
+print "along to ask you questions."
+
+print "See, watch this..."
+
+name = str(raw_input("What is your name?" ))
+age = str(raw_input("How old are you? "))
+location = str(raw_input("Where do you live? "))
+
+print "So your name is %s, you are %s years old, and you live in %s." % (name, age, location)
+
+print "I did all of that with prompts."
+print "Not sure how you could ask me prompts,"
+print "since I don't really exist."
+
+print "Hey, you could look at the code for this"
+print "program and see how I did it."
