81ae06f947fcdddb8a70a4f05c6cfb3c740d5cbb
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-17 18:24:00 -0500

    Just doing the stock example from the book.

---
+++ ex24/practice.py
@@ -0,0 +1,4 @@
+# I'm copping out on this one.  Just follow the textbook
+# exactly and copy the exercise code into practice.py.
+# Technically, you could copy and paste, but that would be
+# cheating :-)  You should still run validate.py when done.
