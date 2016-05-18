bdcc5d069583ac1989c4e51f1e2eba3ca0efc239
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-18 18:28:33 -0500

    Set for testing.

---
+++ ex28/practice.py
@@ -0,0 +1,33 @@
+# Modify the expression sets so that each print
+# comparison will print True
+
+expression_one = True and False
+expression_two =
+
+print expression_one == expression_two
+
+expression_one = (4 > 2) and (3 < 8) 
+expression_two = 
+
+print expression_one == expression_two
+
+expression_one = ('dog' != 'cat') or (1 != 9) 
+expression_two =
+
+print expression_one == expression_two
+
+expression_one =  
+expression_two = (not True or False) or (not True and not False)
+
+print expression_one == expression_two
+
+expression_one = (12 == 12) and ('blank' != 'bank')
+expression_two = 
+
+print expression_one == expression_two
+
+expression_one = True
+expression_two = False
+
+print expression_one == expression_two
+
