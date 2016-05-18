459459788eed0607874b86bd5a0a783da7ae448f
wesbasinger <wbasinger@villagetechschools.org>
2016-05-18 13:52:43 -0500

    Ready for testing.

---
+++ ex27/practice.py
@@ -0,0 +1,107 @@
+# We'll use this opportunity to practice the truth
+# tables that were in the exercise.
+# I've put the truth tables from the exercise below,
+# but I left off the answers.  Finish filling the tables
+# out and define the corresponding row values with
+# variable definitions.
+
+# NOT
+'''
+    NOT   |   TRUE?
+------------------------
+not False |   not_table_row_1
+not True  |   not_table_row_2
+'''
+
+not_table_row_1 =
+not_table_row_2 =
+
+# OR
+'''
+        OR    |   TRUE?
+------------------------
+True or False |   or_table_row_1
+True or True  |   or_table_row_2
+False or True |   or_table_row_3
+False or False|   or_table_row_4
+'''
+
+or_table_row_1 =
+or_table_row_2 =
+or_table_row_3 =
+or_table_row_4 =
+
+# AND
+'''
+        AND     |   TRUE?
+---------------------------
+True and False  |   and_table_row_1
+True and True   |   and_table_row_2
+False and True  |   and_table_row_3
+False and False |   and_table_row_4
+'''
+
+and_table_row_1 =
+and_table_row_2 =
+and_table_row_3 =
+and_table_row_4 =
+
+# NOT OR (also known as NOR)
+'''
+     NOT OR         |   TRUE?
+--------------------------------
+not(True or False)  |   nor_table_row_1
+not(True or True)   |   nor_table_row_2
+not(False or True)  |   nor_table_row_3
+not(False or False) |   nor_table_row_4
+'''
+
+nor_table_row_1 = 
+nor_table_row_2 =
+nor_table_row_3 =
+nor_table_row_4 =
+
+# NOT AND (also known as NAND)
+'''
+	NOT AND			|	TRUE?
+--------------------------------
+not(True and False)  |   nand_table_row_1
+not(True and True)   |   nand_table_row_2
+not(False and True)  |   nand_table_row_3
+not(False and False) |   nand_table_row_4
+'''
+
+nand_table_row_1 = 
+nand_table_row_2 =
+nand_table_row_3 =
+nand_table_row_4 =
+
+# NOT EQUAL
+'''
+	!=		|	TRUE?
+-----------------------
+1 != 0		|	not_equal_row_1
+1 != 1		|	not_equal_row_2
+0 != 1		|	not_equal_row_3
+0 != 0		|	not_equal_row_4
+'''
+
+not_equal_row_1 =
+not_equal_row_2 =
+not_equal_row_3 =
+not_equal_row_4 =
+
+# EQUAL
+'''
+	=		|	TRUE?
+-----------------------
+1 = 0		|	equal_row_1
+1 = 1		|	equal_row_2
+0 = 1		|	equal_row_3
+0 = 0		|	equal_row_4
+'''
+
+equal_row_1 =
+equal_row_2 =
+equal_row_3 =
+equal_row_4 =
