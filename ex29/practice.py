ca47f34f86475dfede3237f144807d1ecb4d729f
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-18 18:56:10 -0500

    Set for testing.

---
+++ ex29/practice.py
@@ -0,0 +1,38 @@
+# Modify the conditional statements in the if blocks
+# so that the file prints
+# For
+# he's
+# a
+# jolly
+# good
+# fellow
+
+if (XXXX):
+	print "Nope"
+
+if (XXXX):
+	print "For"
+
+if (XXXX):
+	print "nails"
+
+if (XXXX):
+	print "he's"
+
+if (XXXX):
+	print "nuts"
+
+if (XXXX):
+	print "a"
+
+if (XXXX):
+	print "jolly"
+
+if (XXXX) or (XXXX):
+	print "dope"
+
+if (XXXX) and (XXXX):
+	print "good"
+
+if (XXX) == (XXX):
+	print "fellow"
