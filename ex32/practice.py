bf206afd74d1cffb5623288eab97558f6dc52c3c
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-19 21:07:13 -0500

    Ready to test.

---
+++ ex32/practice.py
@@ -0,0 +1,36 @@
+# Alright, this is going to be fun. Let's practice
+# with some lists and for loops.
+
+# First, let's define some lists.
+# Make a list below that has every odd number up to 15.
+# Do me a favor and put it in ascending order, order matters on lists.
+
+odds_to_15 = 
+
+# Let's do another one.
+# Make a list of all the colors in the American flag.
+# Don't worry about the order, I'll deal with sorting.
+
+colors_of_flag = 
+
+# And one last list.
+# Make a list that has one string, one integer, 
+# and one Boolean - in that order!
+
+assorted = 
+
+# Now let's practice looping through those lists.
+# write a for loop that will print every element in the odds_to_15 list
+
+
+
+# Make a loop that will print "XXX is a color in the American flag." for
+# every element of colors_of_flag
+
+
+twenties = []
+
+# Write a for loop that will fill twenties with every single
+# number from 20 - 29
+
+
--- ex32/validate.py
+++ ex32/validate.py
@@ -1,20 +1,78 @@
 import unittest
+import subprocess
 
-class TestStringMethods(unittest.TestCase):
+from practice import *
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+proc = subprocess.Popen(
+        ['python', 'practice.py'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+print_text = proc.communicate()[0]
 
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+delim = print_text.split('\n')
+
+class CanCreateListsAndRunForLoops(unittest.TestCase):
+	def test_can_make_a_list_of_odds_to_15(self):
+		self.assertEqual(
+			odds_to_15,
+			[1, 3, 5, 7, 9, 11, 13, 15]
+		)
+	def test_can_make_a_list_of_strings(self):
+		self.assertIn(
+			"red" or "Red",
+			colors_of_flag
+		)
+		self.assertIn(
+			"blue" or "Blue",
+			colors_of_flag
+		)
+		self.assertIn(
+			"white" or "White",
+			colors_of_flag
+		)
+		self.assertEqual(
+			len(colors_of_flag),
+			3
+		)
+	def test_can_make_assorted_list(self):
+		self.assertIsInstance(
+			assorted[0],
+			str
+		)
+		self.assertIsInstance(
+			assorted[1],
+			int
+		)
+		self.assertIsInstance(
+			assorted[2],
+			bool
+		)
+		self.assertEqual(
+			len(assorted),
+			3
+		)
+	def test_can_do_a_basic_loop(self):
+		self.assertEqual(delim[0],"1")
+		self.assertEqual(delim[1],"3")
+		self.assertEqual(delim[2],"5")
+		self.assertEqual(delim[3],"7")
+		self.assertEqual(delim[4],"9")
+		self.assertEqual(delim[5],"11")
+		self.assertEqual(delim[6],"13")
+		self.assertEqual(delim[7],"15")
+
+	def test_can_do_a_simple_loop(self):
+		self.assertEqual(delim[8][-1], ".")
+		self.assertEqual(delim[9][-1], ".")
+		self.assertEqual(delim[10][-1], ".")
+
+	def test_can_fill_a_list_with_for_loop(self):
+		self.assertEqual(
+			twenties,
+			[20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+		)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()   
