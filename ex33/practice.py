f15c344752de9ea97d81dd6e11ed828874cb4877
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 10:13:43 -0500

    Don't know indexes yet, changed the second test.

--- ex33/practice.py
+++ ex33/practice.py
@@ -1,16 +1,9 @@
 # Use a while loop to print all numbers from 1 - 5
 
 
+# Use a while loop to populate the list named empty
+# with all evens 2 - 10
 
-
-# Use a while loop to print all items in the list below
-
-weapons_of_math_instruction = [
-	"protractor",
-	"compass",
-	"calculator",
-	"slide rule",
-	"pencil"
-]
+empty = []
 
 
