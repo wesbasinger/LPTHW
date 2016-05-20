d7ee22af374cabb162ffc6a787b513e6340cf626
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 17:46:56 -0500

    Should be good to go.

--- ex34/validate.py
+++ ex34/validate.py
@@ -4,13 +4,23 @@ from practice import *
 
 class CanAccessListElementsByIndex(unittest.TestCase):
 
-	def test_can_make_5_element_list_with_apple_in_pos_3(test):
+	def test_can_make_5_element_list_with_apple_in_pos_3(self):
 		self.assertEqual(
 			len(five_elements), 5
 		)
 		self.assertEqual(
 			five_elements[3],
 			"apple"
+		)
+
+	def test_can_make_a_list_with_certain_index(self):
+		self.assertEqual(
+			len(four_elements),
+			4
+		)
+		self.assertEqual(
+			four_elements[0],
+			"beginning index"
 		)
 
 	def test_can_access_by_index(self):
