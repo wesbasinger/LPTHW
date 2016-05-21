64fb5b005a2a152a6a371667e0e653eadfe2b3c2
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 19:38:44 -0500

    Ready for testing.

---
+++ ex38/practice.py
@@ -0,0 +1,16 @@
+# First, split this string into a list and assign it to the variable
+# delimited.
+
+quote = "I only regret that I have but one life to lose for my country"
+
+delim = 
+
+# Now (preferably using the .pop method), modify delim so that
+# it does not have the last 5 words of the quote
+
+
+
+# Now, join those back into a string, separated by ~'s.
+# Assign it to the variable joined.
+
+joined =
