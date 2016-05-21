3674dfea9a01ef130e8c5ef560bf8191d20722b1
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 22:37:05 -0500

    Added rstrip() to make pass on Windows.

--- ex01/validate.py
+++ ex01/validate.py
@@ -16,19 +16,19 @@ class TestPrintStatements(unittest.TestCase):
       self.assertEqual(delim[0], 'Your mother was a hamster and your father smelt of elderberries!')
 
   def test_what_have_the_Romans_done_for_us(self):
-      self.assertEqual(delim[1], 'All right... all right... but apart from better sanitation, the medicine, education, wine, public order, irrigation, roads, a fresh water system, and public health ... what have the Romans ever done for us?')
+      self.assertEqual(delim[1].rstrip(), 'All right... all right... but apart from better sanitation, the medicine, education, wine, public order, irrigation, roads, a fresh water system, and public health ... what have the Romans ever done for us?')
 
   def test_happy_occasion_of_killing(self):
-      self.assertEqual(delim[2], 'Please, this is supposed to be a happy occasion. Let us not bicker and argue over who killed who.')
+      self.assertEqual(delim[2].rstrip(), 'Please, this is supposed to be a happy occasion. Let us not bicker and argue over who killed who.')
 
   def test_strange_women_lying_in_ponds(self):
-	  self.assertEqual(delim[3], 'Listen, strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.')
+	  self.assertEqual(delim[3].rstrip(), 'Listen, strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.')
 
   def test_silly_walks(self):
-	  self.assertEqual(delim[4], "I'm sorry to have kept you waiting, but I'm afraid my walk has become rather sillier recently.")
+	  self.assertEqual(delim[4].rstrip(), "I'm sorry to have kept you waiting, but I'm afraid my walk has become rather sillier recently.")
 
   def test_flesh_wound(self):
-	  self.assertEqual(delim[5], "It's just a flesh wound.")	
+	  self.assertEqual(delim[5].rstrip(), "It's just a flesh wound.")	
 
 
 if __name__ == '__main__':
