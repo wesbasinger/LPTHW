c866f49fe7856fb1f3fced2f445c79aa112e65a7
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 20:10:24 -0500

    Ready to test.

---
+++ ex39/practice.py
@@ -0,0 +1,44 @@
+# I'd like you to create 3 dictionaries,
+# one for each of the following words.
+# In the dictionary I want you to give me
+# the length of the word - as an integer
+# If it is a noun - as a boolean
+# If it is a verb - as a boolean
+# all the vowels - as a list
+
+# I'll do one as an example
+
+sky = {
+	"length" : 3,
+	"is_noun" : True,
+	"is_verb" : False,
+	"vowels" : []
+}
+
+grass = {
+	"length" :
+	"is_noun":
+	"is_verb":
+	"vowels" : 
+}
+
+truncate = {
+}
+
+
+# I'm going to define a dictionary below for you.
+
+pets = {
+	"dogs": "long fur",
+	"cats": "short fur",
+	"fish": "scales",
+	"birds": "feathers"
+}
+
+# access the string "feathers" and assign it to the variable soft
+
+soft = 
+
+# access the string "short fur" and assign it to the variable fuzzy
+
+fuzzy =
