d2cb0918ef0f2b4db763b8f26ed2d7d8515f4edc
wesbasinger <wbasinger@villagetechschools.org>
2016-05-20 22:22:24 -0500

    Message was causing failure.

--- ex00/validate.py
+++ ex00/validate.py
@@ -24,11 +24,5 @@ should_string = '''1.  Get a suitable text editor.
 9.  Get comfortable with the command line.
 '''
 
-class TestFileAndDirectoryCreation(unittest.TestCase):
-
-  def test_file_matches(self):
-      self.assertEqual(f.read(), should_string,
-		msg="The text from the files does not match exactly...")
-
 if __name__ == '__main__':
     unittest.main()
