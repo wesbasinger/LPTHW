d36e303e343ec509f94d6680d1596ab509221171
wesbasinger <wbasinger@villagetechschools.org>
2016-05-21 19:36:26 -0500

    Now working on validate.py

---
+++ ex42/practice.py
@@ -0,0 +1,80 @@
+# I'm going to just have you do the exercise, and I'll attempt
+# to verify it for you.  Follow the same pattern as the example.
+# DO NOT ALTER THE LINE NUMBERS, DO NOT ADD EXTRA LINES IN BETWEEN
+
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+## Dog is-a Dog
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## Dog has-a name
+        self.name = name
+
+## ??
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+## ??
+class Person(object):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## ??
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## ??
+        self.salary = salary
+
+## ??
+class Fish(object):
+    pass
+
+## ??
+class Salmon(Fish):
+    pass
+
+## ??
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ??
+satan = Cat("Satan")
+
+## ??
+mary = Person("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish()
+
+## ??
+crouse = Salmon()
+
+## ??
+harry = Halibut()
--- ex42/validate.py
+++ ex42/validate.py
@@ -1,6 +1,43 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+with open('practice.py') as p:
+	for i, line in enumerate(p):
+		if i == 15:
+			line = ln16
+		elif i == 19:
+			line = ln20
+		elif i == 22:
+			line = ln23
+		elif i == 26:
+			line = ln27
+		elif i == 32:
+			line = ln33
+		elif i == 38:
+			line = ln39
+		elif i == 41:
+			line = ln42
+		elif i == 45:
+			line = ln46
+		elif i == 49:
+			line = ln50
+		elif i == 57:
+			line = ln58
+		elif i == 60:
+			line = ln61
+		elif i == 63:
+			line = ln64
+		elif i == 66:
+			line = ln67
+		elif i == 69:
+			line = ln70
+		elif i == 72:
+			line = ln73
+		elif i == 75:
+			line = ln76
+		elif i == 78:
+			line = ln79
+
+class TestKnowsDiffBetweenHasAAndIsA(unittest.TestCase):
 
   def test_upper(self):
       self.assertEqual('foo'.upper(), 'FOO')
