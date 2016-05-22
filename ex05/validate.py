a75cbbb4001ef7371273f216457cab1ca97f44ae
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-21 21:28:16 -0500

    Update validate.py
    Added rstrips

--- ex05/validate.py
+++ ex05/validate.py
@@ -16,38 +16,32 @@ class TestCanUseStringFormatting(unittest.TestCase):
 
     def test_can_print_name_string(self):
 		self.assertEqual(
-			str(delim[0]), 
+			str(delim[0].rstrip()), 
 			"Let's talk about %s." % my_name
 		)
 
     def test_can_print_height_string(self):
 		self.assertEqual(
-			str(delim[1]),
+			str(delim[1].rstrip()),
 			"He's %s inches tall." % my_height
 		)
 
     def test_can_print_weight_string(self):
 		self.assertEqual(
-			str(delim[2]),
+			str(delim[2].rstrip()),
 			"He's %s pounds heavy." % my_weight
 		)
 
     def test_can_print_eyes_and_hair(self):
 		self.assertEqual(
-			str(delim[3]), 
+			str(delim[3].rstrip()), 
 			"He's got %s eyes and %s hair." % (my_eyes, my_hair)
 		)
 
     def test_can_print_teeth_string(self):
 		self.assertEqual(
-			str(delim[4]), 
+			str(delim[4].rstrip()), 
 			"His teeth are usually %s depending on the coffee." % (my_teeth)
-		)
-
-    def test_can_add_variables_and_print(self):
-		self.assertEqual(
-			str(delim[5]),
-			"If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
 		)
 
 
