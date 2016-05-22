ae01136a8afc6027028c2446eeab2a4025220f8a
Wes Basinger <wbasinger@villagetechschools.org>
2016-05-22 07:40:14 -0500

    Added rstrips

--- ex10/validate.py
+++ ex10/validate.py
@@ -16,19 +16,19 @@ class TestCanUseEscapeCharacters(unittest.TestCase):
 
     def test_can_escape_double_quote(self):
 		self.assertEqual(
-			str(delim[0]), 
+			str(delim[0].rstrip()), 
 			"I am 6'2\" tall."
 		)
 
     def test_can_escape_single_quote(self):
 		self.assertEqual(
-			str(delim[1]),
+			str(delim[1].rstrip()),
 			'I am 6\'2" tall.'
 		)
 
     def test_can_escape_a_backslash(self):
 		self.assertEqual(
-			str(delim[2]),
+			str(delim[2].rstrip()),
 			"I'm \\ a \\ cat."
 		)
 
