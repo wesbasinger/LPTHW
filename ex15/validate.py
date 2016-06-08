7263e81ec25b2cefd348422a0c343da1e3d6a600
wesbasinger <wbasinger@villagetechschools.org>
2016-06-08 15:17:11 -0500

    Indentation error.

--- ex15/validate.py
+++ ex15/validate.py
@@ -13,20 +13,17 @@ print_text = proc.communicate()[0]
 delim = print_text.split('\n')
 
 class TestCanOpenFiles(unittest.TestCase):
-
-   def test_can_make_a_file_pointer(self):
-        self.assertIsInstance(
+	def test_can_make_a_file_pointer(self):
+		self.assertIsInstance(
             txt,
             file
         )
-
-    def test_can_read_file_contents(self):
+	def test_can_read_file_contents(self):
 		self.assertEqual(
 			contents[0:3],
 			'DON'
 		)
-
-    def test_can_print_file_to_screen(self):
+	def test_can_print_file_to_screen(self):
 		self.assertEqual(
 			str(delim[41]),
 			"P2: Cuz they're made of... wood?"
