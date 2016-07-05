b5b3ceb53359916081d0313bc7d77c85b91898e6
Wes Basinger <wbasinger@villagetechschools.org>
2016-07-05 14:35:00 -0500

    Add rstrips.

--- ex28/validate.py
+++ ex28/validate.py
@@ -13,27 +13,27 @@ delim = print_text.split('\n')
 class CanTweakBooleanStatments(unittest.TestCase):
 	def test_can_make_a_function_with_one_arg(self):
 		self.assertEqual(
-			delim[0],
+			delim[0].rstrip(),
 			"True"
 		)
 		self.assertEqual(
-			delim[1],
+			delim[1].rstrip(),
 			"True"
 		)
 		self.assertEqual(
-			delim[2],
+			delim[2].rstrip(),
 			"True"
 		)
 		self.assertEqual(
-			delim[3],
+			delim[3].rstrip(),
 			"True"
 		)
 		self.assertEqual(
-			delim[4],
+			delim[4].rstrip(),
 			"True"
 		)
 		self.assertEqual(
-			delim[5],
+			delim[5].rstrip(),
 			"True"
 		)
 
