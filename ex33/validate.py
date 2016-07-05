66aec4ed8d89722e00c901a4608af973332e2b99
Wes Basinger <wbasinger@villagetechschools.org>
2016-07-05 17:23:05 -0500

    Add rstrips.

--- ex33/validate.py
+++ ex33/validate.py
@@ -15,11 +15,11 @@ delim = print_text.split('\n')
 class CanCreateWhileLoops(unittest.TestCase):
 
 	def test_can_use_while_loop_to_print_to_5(self):
-		self.assertEqual(delim[0], "1")
-		self.assertEqual(delim[1], "2")
-		self.assertEqual(delim[2], "3")
-		self.assertEqual(delim[3], "4")
-		self.assertEqual(delim[4], "5")
+		self.assertEqual(delim[0].rstrip(), "1")
+		self.assertEqual(delim[1].rstrip(), "2")
+		self.assertEqual(delim[2].rstrip(), "3")
+		self.assertEqual(delim[3].rstrip(), "4")
+		self.assertEqual(delim[4].rstrip(), "5")
 
 	def test_can_use_while_loop_to_populate_list(self):
 		self.assertEqual(empty, [2, 4, 6, 8, 10])
